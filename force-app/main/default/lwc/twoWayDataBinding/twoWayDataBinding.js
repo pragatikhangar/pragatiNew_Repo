4cc60f932106fe2201d19cce7a70a56a4adfe893
pragatikhangar <pragatikhangar04@gmail.com>
2023-02-23 19:13:47 +0530

    done

--- force-app/main/default/lwc/twoWayDataBinding/twoWayDataBinding.js
+++ force-app/main/default/lwc/twoWayDataBinding/twoWayDataBinding.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
 
-export default class TwoWayDataBinding extends LightningElement {}
+export default class TwoWayDataBinding extends LightningElement {
+    first = "I'm learning  " ;
+    second ="Zero to hero";
+
+    ChangeInput(event)
+    {
+        this.second= event.target.value;
+    }
+}
