1388cc3cc10bd989f7ea8c652599e96d1d4e8a12
pragatikhangar <pragatikhangar04@gmail.com>
2023-02-24 00:53:54 +0530

    lwc

--- force-app/main/default/lwc/twoWayDataBinding/twoWayDataBinding.js
+++ force-app/main/default/lwc/twoWayDataBinding/twoWayDataBinding.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class TwoWayDataBinding extends LightningElement {
     first = "I'm learning  " ;
@@ -6,5 +6,13 @@ export default class TwoWayDataBinding extends LightningElement {
     ChangeInput(event)
     {
         this.second= event.target.value;
+    }
+    @track address= {
+        name: `pragati`,
+        city: `Tumsar`,
+        age: 22
+    }
+    ChangeCity(event){
+        this.address.city = event.target.value;
     }
 }
