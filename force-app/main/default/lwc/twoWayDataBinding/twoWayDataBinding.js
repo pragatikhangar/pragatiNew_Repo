ae7113575269601ba0200a410866b5b666be651c
pragatikhangar <pragatikhangar04@gmail.com>
2023-02-24 00:59:19 +0530

    lwc

--- force-app/main/default/lwc/twoWayDataBinding/twoWayDataBinding.js
+++ force-app/main/default/lwc/twoWayDataBinding/twoWayDataBinding.js
@@ -1,12 +1,14 @@
 import { LightningElement, track } from 'lwc';
 
 export default class TwoWayDataBinding extends LightningElement {
+    //Two way data binding
     first = "I'm learning  " ;
     second ="Zero to hero";
     ChangeInput(event)
     {
         this.second= event.target.value;
     }
+    //@track property
     @track address= {
         name: `pragati`,
         city: `Tumsar`,
