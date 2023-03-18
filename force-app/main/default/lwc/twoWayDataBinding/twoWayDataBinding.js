fcfb16c4496b63efda96e65dc1916982c015d78b
pragatikhangar <pragatikhangar04@gmail.com>
2023-03-19 00:30:23 +0530

    getters

--- force-app/main/default/lwc/twoWayDataBinding/twoWayDataBinding.js
+++ force-app/main/default/lwc/twoWayDataBinding/twoWayDataBinding.js
@@ -17,4 +17,21 @@ export default class TwoWayDataBinding extends LightningElement {
     ChangeCity(event){
         this.address.city = event.target.value;
     }
+    //Getters 
+
+    // Example 1
+    users= [`pragati`, `pk`, `kp`]
+    get firstUser ()
+    {
+      return this.users[0].toUpperCase();
+    }
+
+    //Example 2
+    num1 = 10;
+    num2= 20;
+    get sumOfNumber()
+    {
+        return this.num1 * this.num2;
+    }
+
 }
