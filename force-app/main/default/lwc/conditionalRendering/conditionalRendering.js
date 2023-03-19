fb747a1f25674b74fe7dc24a4aa67f24f9e97640
pragatikhangar <pragatikhangar04@gmail.com>
2023-03-20 00:51:06 +0530

    conditional rendering

--- force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
+++ force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
 
-export default class ConditionalRendering extends LightningElement {}
+export default class ConditionalRendering extends LightningElement 
+{
+    isVisible = false
+    name
+    handleClick(){
+        this.isVisible = true
+    }
+
+    changeHandler(event){
+        this.name = event.target.value
+    }
+
+    get helloMethod(){
+        return this.name === 'hello'
+    }
+
+}
